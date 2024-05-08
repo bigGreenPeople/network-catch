a423853aa3d8cdb96f47eefaf88839349873539a
bigRedPeoPle <1273596620@qq.com>
2024-05-08 22:17:12 +0800

    init

--- popup.js
+++ popup.js
@@ -1,20 +1,41 @@
 console.log("This is a popup!")
 const bg = chrome.extension.getBackgroundPage()
 
-console.log(bg)
+// console.log(bg)
+
+// const intervalId = setInterval(() => {
+//     // 这里写你想要执行的代码
+//     console.log("Interval function executed");
+//     $(function () {
+//         $('#input').val(count);
+//         $('#btn').click(function () {
+//             // bg.toPopup()
+//             // 发送消息给 background 页面
+//             chrome.runtime.sendMessage({ greeting: "Hello from popup" }, response => {
+//                 console.log("Response from background:", response);
+//             });
+//         });
+//     })
+// }, 5000); // 每隔5秒执行一次
 
 
-setInterval(1000,)
-$(function () {
-    $('#input').val(count);
-    $('#btn').click(function () {
-        // bg.toPopup()
-        // 发送消息给 background 页面
+
+setInterval(() => {
+    // 添加点击事件监听器
+    // 获取按钮元素
+    const button = document.getElementById("btn");
+    console.log(button)
+    button.addEventListener("click", function () {
+        // 点击按钮时执行的代码
+        console.log("Button clicked!");
+        
         chrome.runtime.sendMessage({ greeting: "Hello from popup" }, response => {
             console.log("Response from background:", response);
         });
+        // 在这里可以编写其他逻辑，比如加一操作
     });
-})
+}, 3000); // 每隔5秒执行一次
+
 
 // document.getElementById('rBgInfo').onclick = function() {
 //     bg.toPopup()
